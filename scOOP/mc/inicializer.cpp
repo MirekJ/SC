7011133caa7c2775889050825c831e407429cf59
mirekj <mirekj@hat.ncbr.muni.cz>
2017-09-08 10:26:25 +0200

    Remove of exit(1) at warning, induced by some box lenght being smaller then 2 spherocylinders, this way simulation of 2D planes might be problematic. Now just WARNING is printed insted of Warning and quit of program.

--- scOOP/mc/inicializer.cpp
+++ scOOP/mc/inicializer.cpp
@@ -167,15 +167,12 @@ bool Inicializer::initConfig(FILE** infile, std::vector<Particle > &pvec) {
 #endif
     if (conf->geo.box.x < maxlength * 2.0 + 2.0) {
         printf ("WARNING: x (%f) geo.box length is less than two spherocylinders long (%f).\n\n", conf->geo.box.x, maxlength * 2.0 + 2.0);
-        exit(1);
     }
     if (conf->geo.box.y < maxlength * 2.0 + 2.0) {
         printf ("WARNING: y (%f) geo.box length is less than two spherocylinders long (%f).\n\n", conf->geo.box.y, maxlength * 2.0 + 2.0);
-        exit(1);
     }
     if (conf->geo.box.z < maxlength * 2.0 + 2.0) {
         printf ("WARNING: z (%f) geo.box length is less than two spherocylinders long (%f).\n\n", conf->geo.box.z, maxlength * 2.0 + 2.0);
-        exit(1);
     }
 
     DEBUG_INIT("Position of the particle");
